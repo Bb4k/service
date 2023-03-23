ae120b3f78bcb48a1011c7577038c3bb77fc00dc
Victor Andrei Cotescu <victor.cotescu@yahoo.com>
2023-03-24 00:02:37 +0200

    add tests for UserDto

--- src/test/java/ro/unibuc/hello/dto/UserDtoTest.java
+++ src/test/java/ro/unibuc/hello/dto/UserDtoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.times;
@@ -14,6 +15,12 @@ class UserDtoTest {
     @InjectMocks
     UserDto userDtoMock = Mockito.mock(UserDto.class);
     UserDto userDto;
+
+    String testId = "123345test";
+    String testFirstName = "testFirstName";
+    String testLastName = "testLastName";
+    String testPassword = "testPassword";
+    String testEmail = "test@emaill.tst";
 
     @BeforeEach
     void setup() {
@@ -28,22 +35,53 @@ class UserDtoTest {
 
     @Test
     void WHEN_first_name_getter_is_called_THEN_return_correct_first_name_value(){
-        Assertions.assertEquals("1", userDto.getFirstName());
+        Assertions.assertSame("1", userDto.getFirstName());
     }
 
     @Test
     void WHEN_last_name_getter_is_called_THEN_return_correct_last_name_value(){
-        Assertions.assertEquals("1", userDto.getLastName());
+        Assertions.assertSame("1", userDto.getLastName());
     }
 
     @Test
     void WHEN_password_getter_is_called_THEN_return_correct_password_value(){
-        Assertions.assertEquals("1", userDto.getPassword());
+        Assertions.assertSame("1", userDto.getPassword());
     }
 
     @Test
     void WHEN_email_getter_is_called_THEN_return_correct_email_value(){
-        Assertions.assertEquals("1", userDto.getEmail());
+        Assertions.assertSame("1", userDto.getEmail());
     }
+
+    @Test
+    void WHEN_id_is_set_THEN_set_correct_id_value(){
+        userDto.setId(testId);
+        Assertions.assertSame(testId, userDto.getId());
+    }
+
+    @Test
+    void WHEN_first_name_is_set_THEN_set_correct_first_name_value(){
+        userDto.setFirstName(testFirstName);
+        Assertions.assertSame(testFirstName, userDto.getFirstName());
+    }
+
+    @Test
+    void WHEN_last_name_is_set_THEN_set_correct_last_name_value(){
+        userDto.setLastName(testLastName);
+        Assertions.assertSame(testLastName, userDto.getLastName());
+    }
+
+    @Test
+    void WHEN_password_is_set_THEN_set_correct_password_value(){
+        userDto.setPassword(testPassword);
+        Assertions.assertSame(testPassword, userDto.getPassword());
+    }
+
+    @Test
+    void WHEN_email_is_set_THEN_set_correct_email_value(){
+        userDto.setEmail(testEmail);
+        Assertions.assertSame(testEmail, userDto.getEmail());
+    }
+
 
 }
